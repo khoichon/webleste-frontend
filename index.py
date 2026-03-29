916f3409d43cb9483ac7e91003ec4d0c404c38eb
Nguyen Dac Khoi Nguyen <nguyen_dac_khoi_nguyen@students.edu.sg>
2026-03-29 20:14:33 +0800

    Update server to use environment variable for port
    its fine guys

--- index.py
+++ index.py
@@ -5,6 +5,7 @@
 from http.server import SimpleHTTPRequestHandler
 import socketserver
 import sys
+import os
 
 class Handler(SimpleHTTPRequestHandler):
     extensions_map = {
@@ -27,7 +28,7 @@ class Handler(SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    port = int(sys.argv[1]) if len(sys.argv) > 1 else 8000
-    with socketserver.TCPServer(("localhost", port), Handler) as httpd:
+    port = int(os.environ.get("PORT", 8000))
+    with socketserver.TCPServer(("0.0.0.0", port), Handler) as httpd:
         print("Serving on port", port)
         httpd.serve_forever()
